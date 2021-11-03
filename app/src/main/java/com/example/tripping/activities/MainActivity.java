ede6b54f7106d5ad4a55339b8a13d36d29802d01
pavelbogdan71 <pavelbogdan71@gmail.com>
2021-11-03 10:26:02 +0200

    create login inputs and buttons

---
+++ app/src/main/java/com/example/tripping/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tripping.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tripping.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

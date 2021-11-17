9f093dd83ded8e13e66462a03a4b86fee5fa135e
pavelbogdan71 <pavelbogdan71@gmail.com>
2021-11-17 21:31:28 +0200

    map working

--- app/src/main/java/com/example/tripping/activities/MapActivity.java
+++ app/src/main/java/com/example/tripping/activities/MapActivity.java
@@ -1,16 +1,52 @@
 package com.example.tripping.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import com.example.tripping.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private FusedLocationProviderClient client;
+    private SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng currentLocation = new LatLng(0, 0);
+        map.addMarker(new MarkerOptions().position(currentLocation));
+        map.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
     }
 }
